6a28aae0254d2de45f6a7898216347dde3c398e0
Daniel Heinrich <dannynullzwo@gmail.com>
2026-02-22 10:50:03 +0100

    fix csp

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -39,7 +39,7 @@ export default defineConfig({
         ],
       },
       workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,png,svg,webp}"],
+        globPatterns: ["**/*.{js,css,html,ico,svg,webp}"],
         navigateFallback: "/index.html",
         runtimeCaching: [
           {
